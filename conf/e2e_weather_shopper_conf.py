5dde8489d1d0d063a4d9e4568201b5a3c09bf750
Varun <33460576+varun9619@users.noreply.github.com>
2019-11-19 09:34:47 +0530

    made few changes in the code and fixed the bug

--- conf/e2e_weather_shopper_conf.py
+++ conf/e2e_weather_shopper_conf.py
@@ -1,4 +1,6 @@
 """
 This conf contains the data needed by test_e2e_purchase_product.py
 """
-PURCHASE_LOGIC = {"moisturizer":['aloe'],"sunscreen":['spf-50']}
+PURCHASE_LOGIC = {"moisturizers":['Aloe','Almond'],"sunscreens":['SPF-50', 'SPF-30']}  
+#changed moisturizer and sunscreen to moisturizers and sunscreens
+#zadded Almond and SPf-30 to add two items in the cart
